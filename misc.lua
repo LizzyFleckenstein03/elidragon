c05441e5e7d645659a9d72fefda007d5dccb87c6
Elias Fleckenstein <eliasfleckenstein@web.de>
2020-06-22 12:58:28 +0200

    Fixes

--- misc.lua
+++ misc.lua
@@ -7,7 +7,7 @@ end)
 minetest.register_on_joinplayer(function(player)
     minetest.chat_send_player(player:get_player_name(), 
         minetest.colorize("#D6CD00", " ELIDRAGON") .. "\n" ..
-        minetest.colorize("#6076FF"," Join our discord Server (discord.gg/Z7SfXYx) or our IRC channel (#elidragon-skyblocks on irc.edgy1.net)") .. "\n" ..
+        minetest.colorize("#6076FF"," Join our discord Server (discord.gg/vkrmHZc) or our IRC channel (#elidragon-skyblocks on irc.edgy1.net)") .. "\n" ..
         minetest.colorize("#E27900", " Go to hub using /hub") .. "\n" ..
         minetest.colorize("#00F0FF", " Use /island to teleport to your island") .. "\n" ..
         minetest.colorize("#83FF00", " NEWS: ") .. minetest.colorize("#FFFFFF", elidragon.savedata.news or "No current News")
